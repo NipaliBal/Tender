43135778ad06038b8947094273210d846a422003
Nipali <124891630+NipaliBal@users.noreply.github.com>
2023-04-18 14:30:40 +0530

    Delete ProRepository.java

--- ProRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tender.proRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tender.entity.Product;
-
-public interface ProRepository extends JpaRepository<Product, Long> {
-
-}
